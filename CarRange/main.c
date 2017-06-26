59abbb58c718ebfd3290a3cb74322fda7ac707b8
20161104567 <846129391@qq.com>
2017-06-26 11:35:19 +0800

    增加内容
    增加了经纬度，海拔，地面速率，地面航向

--- CarRange/main.c
+++ CarRange/main.c
@@ -13,7 +13,7 @@ int main(int argc, const char * argv[]) {
     FILE *fw;
     int i;
     char s1[70],s2[70];
-    char date[10],time[10],lat[10],lg[10],high[10];
+    char date[10],time[10],lat[10],lg[12],high[10],rate[10],heading[10];
     fr=fopen("//Users//a20161104567//Desktop//CarRange//input.txt","r+");
     fw=fopen("//Users//a20161104567//Desktop//CarRange//output.txt","w+");
     if(fr==NULL)
@@ -43,6 +43,70 @@ int main(int argc, const char * argv[]) {
         printf("时间：%s",time);
         fprintf(fw,"时间：%s",time);
         printf("\t");
+        for(i=0;i<10;i++)
+        {
+            lg[i]=s1[15+i];
+        }
+        lg[10]='\0';
+        printf("纬度：%s",lg);
+        fprintf(fw,"纬度：%s",lg);
+        switch(s1[25])
+        {
+            case 'N':
+                printf("北纬");
+                fprintf(fw,"北纬");
+                break;
+            case 'S':
+                printf("南纬");
+                fprintf(fw,"南纬");
+                break ;
+        }
+        printf("\t");
+        for(i=0;i<9;i++)
+        {
+            lat[i]=s1[28+i];
+        }
+        lat[9]='\0';
+        printf("经度：%s",lat);
+        fprintf(fw,"经度：%s",lat);
+        switch(s1[37])
+        {
+            case 'E':
+                printf("东经");
+                fprintf(fw,"东经");
+                break;
+            case 'W':
+                printf("西经");
+                fprintf(fw,"西经");
+                break ;
+        }
+        printf("\t");
+        for(i=0;i<4;i++)
+        {
+            high[i]=s2[43+i];
+        }
+        high[4]='\0';
+        printf("海拔：%sm",high);
+        fprintf(fw,"海拔：%sm",high);
+        printf("\t");
+        for(i=0;i<5;i++)
+        {
+            rate[i]=s1[39+i];
+        }
+        rate[5]='\0';
+        printf("地面速率：%sm/s",rate);
+        fprintf(fw,"地面速率：%sm/s",rate);
+        printf("\t");
+        for(i=0;i<5;i++)
+        {
+            heading[i]=s1[45+i];
+        }
+        heading[5]='\0';
+        printf("地面航向：%s",heading);
+        fprintf(fw,"地面航向：%s",heading);
+        printf("\t");
+        printf("\n");
+        fprintf(fw,"\n");
         fclose(fr);
         fclose(fw);
     }
